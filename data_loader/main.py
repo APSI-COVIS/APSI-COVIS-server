1c6b46f54316203d4af278fb7ec2a3238db1b687
Terhon <terhon95@gmail.com>
2020-05-26 09:02:14 +0200

    Date datatype

--- data_loader/main.py
+++ data_loader/main.py
@@ -1,6 +1,8 @@
+import os
 import pandas
 from sqlalchemy import create_engine
 
 data_raw = pandas.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv')
-engine = create_engine('postgresql://postgres:123@db:5432/postgres')
+data_raw['Date'] = pandas.to_datetime(data_raw['Date'])
+engine = create_engine('postgresql://' + os.environ['POSTGRES_USER'] + ':' + os.environ['POSTGRES_PASSWORD'] + '@db:5432/' + os.environ['POSTGRES_DB'])
 data_raw.to_sql('covid', engine, if_exists='append')
